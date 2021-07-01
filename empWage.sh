32ddc04999c9e4d0935efbf08eb7527cbe7d412a
DYSTAR14 <ydeepak141097@gmail.com>
2021-07-01 13:43:05 +0530

    Calculate wages for month

--- empWage.sh
+++ empWage.sh
@@ -1,14 +1,19 @@
 #!/bin/bash -x
-empCheck=$((RANDOM%3))
 empRatePerHr=20
-
-case $empCheck in
-	1) empHrs=8
-	;;
-	2) empHrs=4
-	;;
-	*) empHrs=0
-	;;
-esac
-salary=$(($empRatePerHr*$empHrs))
-echo $salary
+numOfworkingDays=10
+totalSalary=0
+for ((day=1;day<=$numOfworkingDays;day++))
+do
+	empCheck=$((RANDOM%3))
+	case $empCheck in
+		1) empHrs=8
+		;;
+		2) empHrs=4
+		;;
+		*) empHrs=0
+		;;
+	esac
+	salary=$(($empRatePerHr*$empHrs))
+	totalSalary=$(($totalSalary+$salary))
+done
+echo $totalSalary
